a4731181ff71a5d020127ca05bd3dac10733e124
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-07 17:47:05 -0300

    should throw error

--- src/lib/calculator.test.js
+++ src/lib/calculator.test.js
@@ -7,3 +7,13 @@ it('should sum 2 and 2 the result must be 4', () => {
 it('should sum 2 and 2 even if one of them is a string and the result must be for', () => {
   expect(sum('2',2)).toBe(4)
 });
+
+it('should throw an error if what is provided to the method cannot be summed', () => {
+  expect(() => {
+    sum('',2)
+  }).toThrowError()
+
+  expect(() => {
+    sum([3,2])
+  }).toThrowError()
+});
