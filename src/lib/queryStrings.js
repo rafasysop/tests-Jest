7350b581ec5bee6a5fae16b75250c10d8519c5aa
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-07 18:03:42 -0300

    test with queryString

---
+++ src/lib/queryStrings.js
@@ -0,0 +1,10 @@
+module.exports.queryString = (obj) => {
+  const string = JSON.stringify(obj)
+    .replaceAll('{', '')
+    .replaceAll('}', '')
+    .replaceAll('"', '')
+    .replaceAll(':', '=')
+    .replaceAll(',', '&')
+
+  return string;
+}
---
+++ src/lib/queryStrings.test.js
@@ -0,0 +1,12 @@
+const { queryString } = require("./queryStrings");
+
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is provided', () => {
+    const obj = {
+      name: 'Rafael',
+      lastName: 'Moura'
+    }
+    queryString(obj)
+    expect(queryString(obj)).toBe("name=Rafael&lastName=Moura")
+  });
+});
