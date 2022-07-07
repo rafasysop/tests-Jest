8facba3811115253b459a8faf96231996bd0157b
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-07 17:32:25 -0300

    second test calculator number with string

--- src/lib/calculator.test.js
+++ src/lib/calculator.test.js
@@ -3,3 +3,7 @@ const { sum } = require("./calculator");
 it('should sum 2 and 2 the result must be 4', () => {
   expect(sum(2,2)).toBe(4)
 });
+
+it('should sum 2 and 2 even if one of them is a string and the result must be for', () => {
+  expect(sum('2',2)).toBe(4)
+});
