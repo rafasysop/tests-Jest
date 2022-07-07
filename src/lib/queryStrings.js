cb2e20591ee2fa31de1d821bc97fdfd9b8299e13
Rafael Moura <rafaelcostamoura@gmail.com>
2022-07-07 19:31:09 -0300

    formatt with prettier

--- src/lib/queryStrings.js
+++ src/lib/queryStrings.js
@@ -1,10 +1,10 @@
 module.exports.queryString = (obj) => {
   const string = JSON.stringify(obj)
-    .replaceAll('{', '')
-    .replaceAll('}', '')
-    .replaceAll('"', '')
-    .replaceAll(':', '=')
-    .replaceAll(',', '&')
+    .replaceAll("{", "")
+    .replaceAll("}", "")
+    .replaceAll('"', "")
+    .replaceAll(":", "=")
+    .replaceAll(",", "&");
 
   return string;
-}
+};
